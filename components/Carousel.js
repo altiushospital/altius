226c6628a10d96c79c7edac184ebd19e21357e9c
altiushospital <poushaliaich@altiushospital.com>
2023-05-20 11:59:09 +0530

    carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,20 +1,18 @@
-import { GraphQLClient } from 'graphql-request';
-import React, { useState,useEffect } from 'react';
-import styles from '../styles/Slider.module.css'
-// import { Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { GraphQLClient } from 'graphql-request'; // Import GraphQLClient
+import styles from '../styles/Slider.module.css';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Image from "next/image";
-// import Form from "./form"
+import Image from 'next/image';
 
+const url = 'https://api-ap-south-1.hygraph.com/v2/clfarqru428bv01t88476g9cn/master';
 
-
-const url="https://api-ap-south-1.hygraph.com/v2/clgfef4yb0cy101t5eon24l0u/master"
-const graphcms = new GraphQLClient(url,{
-    headers:{
-        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODE0MDk0MDQsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmh5Z3JhcGguY29tL3YyL2NsZ2ZlZjR5YjBjeTEwMXQ1ZW9uMjRsMHUvbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6ImFiZTg3NTVjLWNiZGUtNDVmMS05YzM4LTMwN2VlODcxYTdiNyIsImp0aSI6ImNsZ2ZmcXZrZDBkdWUwMXVnZXV1bTJicHQifQ.LkEn61ojGK3Zuq3lu_i_BVdseQtALChPTd--kAUQAisZXZoEla-TfqW_jSMn4FHi-GJ9DwOXLIg7-a_yFShBXJ_wrHbD5kqicltNZcMV1SA23CYLL-0RResyRgACvXJYpSVYkAUNHY_hmRI48b5C3iWGgStZLK8RGc29pg8csPmWWAGre-dzz1x1XZNTyK14V95dJBiVKTSHtVK14XABdjPhwSY-h-aMO6drAboJMWOjyTfdKSUCm8gz8euLTZTrIY7HXc1fgPsGhBJ8SgUkMcbu0lcsc1skJWf5QXj-N6OlKai4aQoItm9ApuzC9gMcSiRXwPhUz9gIDABhbxt3V76dYTlT9aodYlwC0es1-aO2svepGFqrQ3Afd7lphFSiopIxKodhizs_dLcqHdPG_g7DMlYrojdcCuvgFfNpCC7t9OjiGKIHbCsf46B9hQz0zC1A8CS8gNmuVp7kVk5KJS7Ypdo41-bk17SaV28gghNk78RIBa0Wnm_CxFD8VjdFBC77w9mZMer8zyUSwbnVpGV5RcMLG6qDRkZLJ_speLRBwyZx4NPb_5GzFDKCXXM527rBfzfSqP_4bx0srbHqmtoWskd7si7pG1j7KvLAQDYzvlX9xNQ6R08YRuT36mv6m2pS0wTnXhQJqGkhVL1rbJZeRzZ5gZPSRstXiFuLsOo"
-    }
-  })
+const graphcms = new GraphQLClient(url, {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODEzODU2NzcsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmh5Z3JhcGguY29tL3YyL2NsZmFycXJ1NDI4YnYwMXQ4ODQ3Nmc5Y24vbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6IjJmYjM3ZGUwLWFjODUtNGE4MC1hOWIxLTI4NjgxMjMxMDEyMyIsImp0aSI6ImNsZ2YxbWM5aDE3amcwMXVsNm05bjc4NngifQ.vIwPkkque6RIg9VdrWJUqqFllEm4PokDJHh0rSafAJsuw60MMWYx617AoWNo0D_ZdGZiErVobHGIb6jFpPGfEkMy7BEopDGRDBQ2dRcON-YGBezA-8Es3yquF8spaYEkxaYKml4Qr0rxnk0jowqnDFvj729F7lXPRb1CIGHmwNSL96-E3NcR4Z666q-TLdojL8wfSuYj_h_squYH88xtvNc5bY9halE3qQodC2tas3OPuICUafPZmbqcdqHIcenb3bPDhLZHPWai-gUUcdpOz_55MMObsKsWsaXxyAdJz6mznyB_gy-ptd9IKP6bv2F8LvnG1sBxXhTiv_Wdh0OaYOP21cgVrx2uhXe0ebg2AZG4utbJ1LPrHtsOraehyC2phtRfvYK7Qd2oClP6ItRaTGt6glPgIBOfgBbv3P_eLA7Sr7IkMNYUpr26ackc1pSx0rr3-dqAFr1vmFUbsD6a_JKm2MYEiQ8gKSZXiE0uW0xTH9cOu6AXoqUAH8Ii6pGW-BcosYkxnn4oXvKdt4ET81ZjH6oGmjc4rk7pXio9udGAVXgyp6gJcNQRp_5aksxLkwdKfNHiuGAeEK7kGeri0D3MrAXfb3g__ghbfRCTfgvjPVIQtldCDslTWPvXsbrOztXTIBNo5Tlxv8d4UTuiZ6DkUdmk9YUJS2Jf8wjEBx8',
+  },
+});
 
 export default function SliderDemo() {
   const [sliders, setCarouselImages] = useState([]);
@@ -22,19 +20,18 @@ export default function SliderDemo() {
   useEffect(() => {
     async function fetchCarouselImages() {
       const query = `{
-        sliders {
+        carousels {
           id
-          fertilitytext
-          fertilityCarousel{
+          text
+          carousel {
             url
           }
-          
         }
       }`;
 
       try {
-        const { sliders } = await graphcms.request(query);
-        setCarouselImages(sliders);
+        const { carousels } = await graphcms.request(query);
+        setCarouselImages(carousels);
       } catch (error) {
         console.error(error);
       }
@@ -42,22 +39,23 @@ export default function SliderDemo() {
 
     fetchCarouselImages();
   }, []);
-  console.log(sliders)
 
   return (
-   
     <div>
-     <Carousel className={styles.slider}>
-      {!!sliders && Array.isArray(sliders) && sliders.map((image) => (
-        <Carousel.Item key={sliders.id} className={styles.sliderItem}>
-        
-                <Image src={image.fertilityCarousel[0].url} className={styles.carouselimg} key={image.id} alt={image.fertilitytext} height={600} width={1600}/>
-                
-                {/* <Form/> */}
-              
-                </Carousel.Item>
-        
-      ))}
+      <Carousel className={styles.slider}>
+        {!!sliders &&
+          Array.isArray(sliders) &&
+          sliders.map((image) => (
+            <Carousel.Item key={image.id} className={styles.sliderItem}>
+              <Image
+                src={image.carousel[0].url}
+                className={styles.carouselimg}
+                alt={image.text}
+                height={600}
+                width={1600}
+              />
+            </Carousel.Item>
+          ))}
       </Carousel>
     </div>
   );
