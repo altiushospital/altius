12c2dce25653d0f7300887ff0dfd89339269e962
altiushospital <poushaliaich@altiushospital.com>
2023-05-20 13:10:16 +0530

    infrastructure

--- components/Banner2.js
+++ components/Banner2.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { GraphQLClient } from 'graphql-request'; 
 import styles from '../styles/Banner2.module.css';
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const Banner2 = () => {
+
+  const url = 'https://api-ap-south-1.hygraph.com/v2/clfarqru428bv01t88476g9cn/master';
+
+  const graphcms = new GraphQLClient(url, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODEzODU2NzcsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmh5Z3JhcGguY29tL3YyL2NsZmFycXJ1NDI4YnYwMXQ4ODQ3Nmc5Y24vbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6IjJmYjM3ZGUwLWFjODUtNGE4MC1hOWIxLTI4NjgxMjMxMDEyMyIsImp0aSI6ImNsZ2YxbWM5aDE3amcwMXVsNm05bjc4NngifQ.vIwPkkque6RIg9VdrWJUqqFllEm4PokDJHh0rSafAJsuw60MMWYx617AoWNo0D_ZdGZiErVobHGIb6jFpPGfEkMy7BEopDGRDBQ2dRcON-YGBezA-8Es3yquF8spaYEkxaYKml4Qr0rxnk0jowqnDFvj729F7lXPRb1CIGHmwNSL96-E3NcR4Z666q-TLdojL8wfSuYj_h_squYH88xtvNc5bY9halE3qQodC2tas3OPuICUafPZmbqcdqHIcenb3bPDhLZHPWai-gUUcdpOz_55MMObsKsWsaXxyAdJz6mznyB_gy-ptd9IKP6bv2F8LvnG1sBxXhTiv_Wdh0OaYOP21cgVrx2uhXe0ebg2AZG4utbJ1LPrHtsOraehyC2phtRfvYK7Qd2oClP6ItRaTGt6glPgIBOfgBbv3P_eLA7Sr7IkMNYUpr26ackc1pSx0rr3-dqAFr1vmFUbsD6a_JKm2MYEiQ8gKSZXiE0uW0xTH9cOu6AXoqUAH8Ii6pGW-BcosYkxnn4oXvKdt4ET81ZjH6oGmjc4rk7pXio9udGAVXgyp6gJcNQRp_5aksxLkwdKfNHiuGAeEK7kGeri0D3MrAXfb3g__ghbfRCTfgvjPVIQtldCDslTWPvXsbrOztXTIBNo5Tlxv8d4UTuiZ6DkUdmk9YUJS2Jf8wjEBx8',
+    },
+  });
+  
+    const [banners, setImages] = useState([]);
+  
+    useEffect(() => {
+      async function fetchCarouselImages() {
+        const query = `{
+          banners {
+            id
+            alttext
+            banner {
+              url
+            }
+          }
+        }`;
+  
+        try {
+          const { banners } = await graphcms.request(query);
+          setImages(banners);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+  
+      fetchCarouselImages();
+    }, []);
+
   return (
     <div className={styles.bannerwrap}>
       <div className={styles.contentWrap}>
-        <div className={styles.imageWrap}>
-          <Image
-            className={styles.bannerimg}
-            height={1500}
-            width={800}
-            src="/images/temp/bannertemp.webp"
-            alt="Card img"
-          />
-          <span className={styles.imgspan}>25+ years of experience</span>
-        </div>
+      {!!banners &&
+          Array.isArray(banners) &&
+          banners.map((image) => (
+            <div className={styles.imageWrap} key={image.id}>
+              <Image
+                src={image.banner[0].url}
+                className={styles.bannerimg}
+                alt={image.text}
+                height={500}
+                width={800}
+              />
+              <span className={styles.imgspan}>25+ years of experience</span>
+            </div>
+          ))}
 
         <div className={styles.textWrap}>
           <h1 className={styles.bannerheader}>Altius Hospitals - A Place Safer Than Home</h1>
--- components/Sliderinfrastructure.js
+++ components/Sliderinfrastructure.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 import Image from 'next/image';
-import styles from '../styles/Sliderinfrastructure.module.css'; // Import custom CSS styles
+import styles from '../styles/Sliderinfrastructure.module.css';
 
 const Sliderinfrastructure = () => {
-  const images = [
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-    "/images/temp/bannertemp.webp",
-  ];
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch('https://api-ap-south-1.hygraph.com/v2/clfarqru428bv01t88476g9cn/master', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODEzODU2NzcsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmh5Z3JhcGguY29tL3YyL2NsZmFycXJ1NDI4YnYwMXQ4ODQ3Nmc5Y24vbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6IjJmYjM3ZGUwLWFjODUtNGE4MC1hOWIxLTI4NjgxMjMxMDEyMyIsImp0aSI6ImNsZ2YxbWM5aDE3amcwMXVsNm05bjc4NngifQ.vIwPkkque6RIg9VdrWJUqqFllEm4PokDJHh0rSafAJsuw60MMWYx617AoWNo0D_ZdGZiErVobHGIb6jFpPGfEkMy7BEopDGRDBQ2dRcON-YGBezA-8Es3yquF8spaYEkxaYKml4Qr0rxnk0jowqnDFvj729F7lXPRb1CIGHmwNSL96-E3NcR4Z666q-TLdojL8wfSuYj_h_squYH88xtvNc5bY9halE3qQodC2tas3OPuICUafPZmbqcdqHIcenb3bPDhLZHPWai-gUUcdpOz_55MMObsKsWsaXxyAdJz6mznyB_gy-ptd9IKP6bv2F8LvnG1sBxXhTiv_Wdh0OaYOP21cgVrx2uhXe0ebg2AZG4utbJ1LPrHtsOraehyC2phtRfvYK7Qd2oClP6ItRaTGt6glPgIBOfgBbv3P_eLA7Sr7IkMNYUpr26ackc1pSx0rr3-dqAFr1vmFUbsD6a_JKm2MYEiQ8gKSZXiE0uW0xTH9cOu6AXoqUAH8Ii6pGW-BcosYkxnn4oXvKdt4ET81ZjH6oGmjc4rk7pXio9udGAVXgyp6gJcNQRp_5aksxLkwdKfNHiuGAeEK7kGeri0D3MrAXfb3g__ghbfRCTfgvjPVIQtldCDslTWPvXsbrOztXTIBNo5Tlxv8d4UTuiZ6DkUdmk9YUJS2Jf8wjEBx8',
+        },
+        body: JSON.stringify({
+          query: `
+            query {
+              galleries {
+                id
+                alttext
+                img {
+                  url
+                }
+              }
+            }
+          `,
+        }),
+      });
+
+      const { data } = await response.json();
+      setImages(data.galleries);
+    }
+
+    fetchImages();
+  }, []);
 
   const items = [];
 
   for (let i = 0; i < images.length; i += 2) {
     const item = (
-      <Carousel.Item key={i}>
+      <Carousel.Item key={images[i].id}>
         <div className={`d-flex justify-content-center ${styles.carouselItem}`}>
           <div className={`carousel-image ${styles.carouselImage}`}>
-            <Image src={images[i]} alt={`Slide ${i}`} width={600} height={400} />
+            <Image src={images[i].img[0].url} alt={images[i].alttext} width={600} height={400} />
           </div>
           {i + 1 < images.length && (
             <div className={`carousel-image ${styles.carouselImage}`}>
-              <Image src={images[i + 1]} alt={`Slide ${i + 1}`} width={600} height={400} />
+              <Image src={images[i + 1].img[0].url} alt={images[i + 1].alttext} width={600} height={400} />
             </div>
           )}
         </div>
@@ -42,13 +59,13 @@ const Sliderinfrastructure = () => {
 
   return (
     <>
-    <Carousel interval={null} indicators={true} prevIcon={<span className={`carousel-control-prev-icon ${styles.carouselControlIcon}`} />} nextIcon={<span className={`carousel-control-next-icon ${styles.carouselControlIconleft}`} />}>
-      {items}
-    </Carousel>
-                <div className={styles.containerbtnwrap}>
-                <button className={styles.morebtn}>{"Visit Today →"}</button>
-                </div>
-                </>
+      <Carousel interval={null} indicators={true} prevIcon={<span className={`carousel-control-prev-icon ${styles.carouselControlIcon}`} />} nextIcon={<span className={`carousel-control-next-icon ${styles.carouselControlIconleft}`} />}>
+        {items}
+      </Carousel>
+      <div className={styles.containerbtnwrap}>
+        <button className={styles.morebtn}>{"Visit Today →"}</button>
+      </div>
+    </>
   );
 };
 
